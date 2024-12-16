eb02c983ef24b967959af4a101af6b3b8c579e76
Abdullah khan <akhan283785@gmail.com>
2024-12-16 11:21:39 +0530

    try

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let xW = document.querySelector(".x");
+let yH = document.querySelector(".y");
+
+window.addEventListener("resize", (e) => {
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  xW.innerHTML = width;
+  yH.innerHTML = height;
+});
